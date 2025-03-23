29923dc4a0d535ad6d2692b88a790350db6d36bb
unknown <mkmk@email.cz>
2025-03-23 18:49:35 +0100

    params

--- scan.ps1
+++ scan.ps1
@@ -21,43 +21,79 @@ if( $deviceManager.DeviceInfos.Count -eq 0)
 $device = $deviceManager.DeviceInfos.Item(1).Connect()
 
 
+#---------------------
+
 # https://docs.microsoft.com/en-us/windows/win32/wia/-wia-wia-property-constant-definitions
+# https://github.com/tpn/winsdk-7/blob/master/v7.1A/Include/WiaDef.h
 
-$device.Items(1).Properties("6147").Value = $dpi   # horizontal DPI
-$device.Items(1).Properties("6148").Value = $dpi   # vertical DPI
 
-$device.Items(1).Properties("6149").Value = 0  # x point where to start scan
-$device.Items(1).Properties("6150").Value = 0  # y point where to start scan
+# horizontal DPI
+$device.Items(1).Properties("6147").Value = $dpi
 
+
+# vertical DPI
+$device.Items(1).Properties("6148").Value = $dpi
+
+
+# X/Y pivot
+$device.Items(1).Properties("6149").Value = 0
+$device.Items(1).Properties("6150").Value = 0
+
+# width
 $device.Items(1).Properties("6151").Value = [int]$widthPx 
+
+# height
 $device.Items(1).Properties("6152").Value = [int]$heightPx 
 
-# $device.Items(1).Properties("6146").Value = 2   # colors
-# $device.Items(1).Properties("4104").Value = 8   # bits per pixel
+# intent
+# https://learn.microsoft.com/cs-cz/windows-hardware/drivers/image/wia-ips-cur-intent
+$device.Items(1).Properties("6146").Value = 0x00000001 -bor 0x00020000
+
+# data type
+# https://learn.microsoft.com/en-us/windows-hardware/drivers/image/wia-ipa-datatype
+$device.Items(1).Properties("4103").Value = 3
+
+# bits per pixel
+$device.Items(1).Properties("4104").Value = 24
+
+# contrast
+# https://learn.microsoft.com/en-us/windows-hardware/drivers/image/wia-ips-contrast
+$device.Items(1).Properties("6155").Value = 0
+
+# brightness
+# https://learn.microsoft.com/en-us/windows-hardware/drivers/image/wia-ips-brightness
+$device.Items(1).Properties("6154").Value = 0
 
 
-### Scan the image from scanner as BMP
+#---------------------
+
+$wiaFormatBmp  = "{B96B3CAB-0728-11D3-9D7B-0000F81EF32E}"
+$wiaFormatPng  = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}"
+$wiaFormatGif  = "{B96B3CB0-0728-11D3-9D7B-0000F81EF32E}"
+$wiaFormatJpeg = "{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}"
+$wiaFormatTiff = "{B96B3CB1-0728-11D3-9D7B-0000F81EF32E}"
+
+
+# https://stackoverflow.com/questions/25371269/scan-automation-with-powershell-and-wia-how-to-set-png-as-image-type
+
+
 foreach ($item in $device.Items)
 {
-    $image = $item.Transfer() 
+    $image = $item.Transfer($wiaFormatJpeg) 
 }
 
 
 ### process scanned image
-$BMP  = "{B96B3CAB-0728-11D3-9D7B-0000F81EF32E}"
-$PNG  = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}"
-$GIF  = "{B96B3CB0-0728-11D3-9D7B-0000F81EF32E}"
-$JPEG = "{B96B3CAE-0728-11D3-9D7B-0000F81EF32E}"
-$TIFF = "{B96B3CB1-0728-11D3-9D7B-0000F81EF32E}"
+# $imageProcess = new-object -ComObject WIA.ImageProcess
+# $imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
+# $imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = [string]$wiaFormatJpeg
+# $imageProcess.Filters.Item(1).Properties.Item("Quality").Value = $quality
+# $image = $imageProcess.Apply($image)
 
-$imageProcess = new-object -ComObject WIA.ImageProcess
-$imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
-$imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = [string]$JPEG
-$imageProcess.Filters.Item(1).Properties.Item("Quality").Value = $quality
-$image = $imageProcess.Apply($image)
 
 # $folder = $([Environment]::GetFolderPath("Desktop"))
 $folder = "."
+
 
 $timestamp = New-TimeSpan "01 January 1970 00:00:00"
 $timestamp = $timestamp.TotalSeconds
@@ -70,4 +106,5 @@ Write-Host $filename
 
 ### Show image 
 # & $filename
+
 
